c8c5a5e596a644e9132fe80524103102998a864c
Richard Opálka <opalka.richard@gmail.com>
2025-07-02 13:47:52 +0200

    WIP - refactoring. Removing duplicated closure of 'in' input stream. It is safely closed in associated doneCallback on next line.

--- common/src/main/java/org/wildfly/httpclient/common/HttpTargetContext.java
+++ common/src/main/java/org/wildfly/httpclient/common/HttpTargetContext.java
@@ -298,7 +298,6 @@ public class HttpTargetContext extends AbstractAttachable {
                                         failureHandler.handleFailure(HttpClientMessages.MESSAGES.invalidResponseCode(response.getResponseCode(), response));
                                         //close the connection to be safe
                                         connection.done(true);
-
                                     } else {
                                         if (httpResultHandler != null) {
                                             final InputStream in = new WildflyClientInputStream(result.getConnection().getBufferPool(), result.getResponseChannel());
@@ -311,7 +310,6 @@ public class HttpTargetContext extends AbstractAttachable {
                                                 connection.done(false);
                                             };
                                             if (response.getResponseCode() == NO_CONTENT) {
-                                                IoUtils.safeClose(in);
                                                 httpResultHandler.handleResult(null, response, doneCallback);
                                             } else {
                                                 String encoding = getResponseHeader(response, CONTENT_ENCODING);
@@ -334,7 +332,6 @@ public class HttpTargetContext extends AbstractAttachable {
                                             connection.done(false);
                                         }
                                     }
-
                                 } catch (Exception e) {
                                     try {
                                         failureHandler.handleFailure(e);
