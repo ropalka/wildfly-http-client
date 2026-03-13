2d76c1e83c692ae1b26ead591dd7ef83317906ae
Richard Opálka <opalka.richard@gmail.com>
2026-03-13 14:02:13 +0100

    [WEJBHTTP-153] Copying ejb-remote jakarta example to javax folder

---
+++ interops/javax/ejb-remote/execute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "${JBOSS_HOME}" ]; then
+    echo "WARNING: JBOSS_HOME is not set."
+    exit 1
+fi
+
+CP="${CP}:$JBOSS_HOME/bin/client/jboss-client.jar"
+CP="${CP}:target/libs/hamcrest-core.jar"
+CP="${CP}:target/libs/junit.jar"
+CP="${CP}:target/test-classes"
+CP="${CP}:target/classes"
+
+#DEBUG="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+PROPS="-Dremoting.over.http=false"
+
+$JAVA_HOME/bin/java -cp $CP $DEBUG $PROPS org.junit.runner.JUnitCore org.jboss.as.quickstarts.ejb.remote.EJBRemoteIT
